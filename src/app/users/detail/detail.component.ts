5db7fd47d0e4bc31dd294a65ae268708d6be23cd
Eduardo Sotero <eduardoliberatosotero@gmail.com>
2018-06-05 21:16:34 -0300

    Saving changes

--- src/app/users/detail/detail.component.ts
+++ src/app/users/detail/detail.component.ts
@@ -1,5 +1,8 @@
+import { UsersService } from './../users.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,5 +12,64 @@ import { Component } from '@angular/core';
 })
 export class DetailComponent {
 
-  constructor() { }
+  form: FormGroup;
+  isEdit: boolean;
+  roles: any[];
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private service: UsersService,
+    private route: ActivatedRoute
+  ) {
+
+    this.roles = [
+      { id: 'ADMIN', name: 'Admin' },
+      { id: 'NORMAL', name: 'Normal user' }
+    ];
+
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role: ['', Validators.required]
+    });
+
+    this.route.params.subscribe(params => {
+      if (Object.keys(params).length > 0) {
+        this.service.getById(params.id).subscribe((res: any) => {
+          this.isEdit = true;
+          Object.keys(res.data).forEach((i) => {
+            if (i !== 'id' && this.form.controls[i]) {
+              this.form.controls[i].setValue(res.data[i]);
+            }
+          });
+        });
+      }
+    });
+
+  }
+
+  goBack() {
+    this.router.navigate(['/app/users']);
+  }
+
+  cancel() {
+    this.goBack();
+  }
+
+  save(form: FormGroup) {
+    this.route.params.subscribe(params => {
+      if (Object.keys(params).length > 0) {
+        this.service.edit(params.id, form.value).subscribe(() => {
+          this.goBack();
+        });
+      } else {
+        this.service.add(form.value).subscribe(() => {
+          this.goBack();
+        });
+      }
+    });
+  }
+
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UsersComponent } from './users.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UsersRoutes } from './users.routing';
 import { MaterialModule } from '../core/material.module';
 
@@ -10,6 +11,8 @@ import { MaterialModule } from '../core/material.module';
   imports: [
     CommonModule,
     MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(UsersRoutes)
   ],
   declarations: [UsersComponent, DetailComponent]
